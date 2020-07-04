e959607ab7158cded2ec8be12eaa4ce1198ba304
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-07-04 20:18:44 +0000

    created getAllContact function

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,27 @@
 const getState = ({ getStore, setStore }) => {
+	const APIurl = "https://assets.breatheco.de/apis/fake/contact/agenda/pluco";
 	return {
 		store: {
+			contacts: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
+			getAllContacts: async () => {
+				try {
+					let response = await fetch(APIurl);
+					if (response.ok) {
+						contacts = await response.json();
+					} else if (response.stats == 404) {
+						alert("no contacts found!!");
+					}
+				} catch (error) {
+					console.log("something failed");
+					console.log(error);
+				}
+				setStore({
+					contacts: contacts
+				});
+			}
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
